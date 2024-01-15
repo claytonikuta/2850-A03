1165d816dff6194ae9da8c5783015b71e062fef2
Charlotte Chak <cchak4@my.bcit.ca>
2024-01-15 15:21:24 -0800

    nav bar show

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,12 @@
 $(function() {
     function slideMenu() {
-      var activeState = $("#menu-container .menu-list").hasClass("active");
-      $("#menu-container .menu-list").animate({left: activeState ? "0%" : "-100%"}, 400);
+      var activeState = $(".nav-mobile-container").hasClass("active");
+      // $("#menu-container .nav-mobile-conatainer").css({left: activeState ? "0%" : "-100%"}, 400);
     }
     $("#menu-wrapper").click(function(event) {
       event.stopPropagation();
-      $("#hamburger-menu").toggleClass("open");
-      $("#menu-container .menu-list").toggleClass("active");
-      slideMenu();
+      $(".nav-mobile-container").toggleClass("active");
+      // slideMenu();
   
       $("body").toggleClass("overflow-hidden");
     });
