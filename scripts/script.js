d7d5d747a1752970708078c2e1df975f6c815d0b
alexfang324 <106351391+alexfang324@users.noreply.github.com>
2024-01-15 16:01:58 -0800

    saving checkpoing

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,48 @@
-$(function() {
-    function slideMenu() {
-      var activeState = $("#menu-container .menu-list").hasClass("active");
-      $("#menu-container .menu-list").animate({left: activeState ? "0%" : "-100%"}, 400);
-    }
-    $("#menu-wrapper").click(function(event) {
-      event.stopPropagation();
-      $("#hamburger-menu").toggleClass("open");
-      $("#menu-container .menu-list").toggleClass("active");
-      slideMenu();
-  
-      $("body").toggleClass("overflow-hidden");
-    });
-  
+$(function () {
+  function slideMenu() {
+    var activeState = $('#menu-container .menu-list').hasClass('active');
+    $('#menu-container .menu-list').animate(
+      { left: activeState ? '0%' : '-100%' },
+      400
+    );
+  }
+  $('#menu-wrapper').click(function (event) {
+    event.stopPropagation();
+    $('#hamburger-menu').toggleClass('open');
+    $('#menu-container .menu-list').toggleClass('active');
+    slideMenu();
 
-  }); // jQuery load
-  
+    $('body').toggleClass('overflow-hidden');
+  });
+
+  //onHover function
+  $(document).on('mouseover', '.media-content', function () {
+    $(this).find('.image').css('display', 'none');
+    $(this).find('.video').css('display', 'block');
+  });
+
+  //play video on hover
+  $(document).on('mouseover', 'video', function () {
+    $(this).get(0).muted = true;
+    $(this).get(0).load();
+    $(this).get(0).play();
+  });
+
+  //pause video on mouse leave
+  $(document).on('mouseleave', 'video', function () {
+    $(this).get(0).currentTime = 0;
+    $(this).get(0).pause();
+    //hideVideo
+    $(this).css('display', 'none');
+    $(this).find('.image').css('display', 'block');
+  });
+
+  //show video controls on Click
+  $(document).on('click', 'video', function () {
+    if ($(this).attr('controls')) {
+      $(this).removeAttr('controls');
+    } else {
+      $(this).attr('controls', '');
+    }
+  });
+}); // jQuery load
